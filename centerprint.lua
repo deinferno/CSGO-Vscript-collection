91078adae1a3f3400af39ec8b38ba2c5655d107e
deinferno <daniil.shukshin@gmail.com>
2015-09-21 14:09:12 +0500

    Center Print

---
+++ centerprint.lua
@@ -0,0 +1,24 @@
+::env_hudhint<-Entities.CreateByClassname("env_hudhint")
+
+::CenterPrintText<-function(player,text){
+env_hudhint.__KeyValueFromString("message",text)
+DoEntFire("!self","ShowHudHint",text,0.0,player,env_hudhint)
+}
+
+::CenterPrintTextTeam<-function(team,text){
+env_hudhint.__KeyValueFromString("message",text)
+player <- null;
+while((player = Entities.FindByClassname(player,"player")) != null){
+if (team==player.GetTeam()){
+DoEntFire("!self","ShowHudHint",text,0.0,player,env_hudhint)
+}
+}
+}
+
+::CenterPrintTextAll<-function(text){
+env_hudhint.__KeyValueFromString("message",text)
+player <- null;
+while((player = Entities.FindByClassname(player,"player")) != null){
+DoEntFire("!self","ShowHudHint",text,0.0,player,env_hudhint)
+}
+}
